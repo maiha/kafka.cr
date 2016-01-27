70da3fc52ecf9d12ac44ee373049a0f546de110f
maiha <maiha@wota.jp>
2016-01-28 01:09:10 +0900

    fix help

--- src/main/heartbeat.cr
+++ src/main/heartbeat.cr
@@ -4,8 +4,30 @@ require "../kafka"
 require "./utils/*"
 
 dump = false
+usage = false
 opts = OptionParser.parse(ARGV) do |parser|
-  parser.on("-d", "--dump", "Dump octal raw data to stdout") { dump = true }
+  parser.on("-d", "--dump", "Dump octal data") { dump = true }
+  parser.on("-h", "--help", "Show this help") { usage = true }
+end
+
+help = -> {
+  puts "Usage: kafka-heartbeat [options] destination"
+  puts "  A destination is `host:port` or `host` (default port: 9092)"
+  puts ""
+  puts "Options:"
+  puts opts
+  puts "\n"
+  puts <<-EXAMPLE
+    Example:
+      #{$0} localhost
+      #{$0} localhost:9092
+      #{$0} -d localhost
+    EXAMPLE
+  exit
+}
+
+if usage || ARGV.empty?
+  help.call
 end
 
 broker = Kafka::Cluster::Broker.parse(ARGV.shift.not_nil!)
