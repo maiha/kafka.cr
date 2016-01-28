c55a2ca4a3cc73bdbd79a8e85633e6ec4d639687
maiha <kazunori_nishi@dwango.co.jp>
2016-01-28 18:14:17 +0900

    fix

--- src/kafka/cluster/broker.cr
+++ src/kafka/cluster/broker.cr
@@ -9,9 +9,9 @@ class Kafka::Cluster::Broker
     
   def self.parse(str : String) : Broker
     case str
-    when /\A[a-zA-Z0-9_\.]+\Z/
+    when /\A[a-zA-Z0-9_\-\.]+\Z/
       new(str)
-    when /\A([a-zA-Z0-9_\.]+):(\d+)\Z/
+    when /\A([a-zA-Z0-9_\-\.]+):(\d+)\Z/
       new($1, $2.to_i)
     else
       raise "Unable to parse #{str} to a broker"
